e0c5fe136a263ffa18bde9eb98a1055ebc699acb
AnhND <anh@gmail.com>
2026-05-24 20:52:25 +0700

    Update

--- eval_ada_3bit_all.sh
+++ eval_ada_3bit_all.sh
@@ -144,10 +144,10 @@ eval_model() {
 # =============================================================
 #eval_model "Llama-3-8B" \
 #    "/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3-8B/snapshots/8cde5ca8380496c9a6cc7ef3a8b46a0372a1d920"
-eval_model "Meta-Llama-3.1-8B" \
-    "/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b"
 eval_model "Mistral-7B-v0.3" \
     "/home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1"
+eval_model "Meta-Llama-3.1-8B" \
+    "/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b"
 #eval_model "Qwen2.5-7B" \
 #    "/home/DATA/prometheus/anh/.cache/huggingface/hub/models--Qwen--Qwen2.5-7B/snapshots/d149729398750b98c0af14eb82c78cfe92750796"
 
